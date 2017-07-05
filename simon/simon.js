1ed3c0dbe7b7a5055f0310004e4ca1c61237d5e8
Matthew Chan <mchan393@uwo.ca>
2017-07-05 15:58:55 -0400

    adding readme for tic tac toe and future simon project

---
+++ simon/simon.js
@@ -0,0 +1,8 @@
+//each colour squares will be given a value from 0 to 3
+//randomize it
+
+//play it 
+
+//after each iteration, add 1, so that you can play 2, then 3, ... sounds
+
+//speed up at 5, 9, 13
