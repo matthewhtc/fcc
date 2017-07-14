2a1cffa3cb32ff3d8c0fb15dff6c9ec7c0d9ae82
Matthew Chan <mchan393@uwo.ca>
2017-07-14 10:23:41 -0400

    added hover effect for colours

--- simon/simon.js
+++ simon/simon.js
@@ -1,7 +1,20 @@
+var count = 1; 
+
+
+
+function getRandom(min, max) {
+	//if you ever don't understand this math, just think about the math again
+	//rmb that math.random() returns a number between 0 and 1 exclusive
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 //each colour squares will be given a value from 0 to 3
 //randomize it
+$(document).on("click", ".quarter", function() {
+	console.log(Number($(this).attr("value")));
+	$(".top-left").addClass("top-left-active"); 
 
+}); 
 //play it 
 
 //after each iteration, add 1, so that you can play 2, then 3, ... sounds
